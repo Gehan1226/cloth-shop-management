2660835ed1632982384675e05c31afe2711b6810
Gehan1226 <gehansithija2023@gmail.com>
2024-06-14 07:57:51 +0530

    add user registration form

--- src/main/java/org/example/controller/AdminDashboardFormController.java
+++ src/main/java/org/example/controller/AdminDashboardFormController.java
@@ -1,4 +1,8 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+
 public class AdminDashboardFormController {
+    public void btnMainmenuOnAction(ActionEvent actionEvent) {
+    }
 }
