5eb8df132a68e221b93f4bcb080be1201ffd2ab5
Gehan1226 <gehansithija2023@gmail.com>
2024-06-16 19:31:52 +0530

    add supplier form controller

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -79,3 +79,5 @@ d
 4src/main/java/org/example/entity/SupplierEntity.java,9\0\901ea8d2d3be826ff9e0020a2366406cc670ef57
 `
 0src/main/java/org/example/entity/ItemEntity.java,d\b\db6c6a3e0183c3770cbc7eedc97141b9847b3181
+t
+Dsrc/main/java/org/example/controller/AddSupplieerFormcontroller.java,3\e\3e77b99ecbbb20636659883bb7b400f318efa81d
--- src/main/java/org/example/controller/AddSupplieerFormcontroller.java
+++ src/main/java/org/example/controller/AddSupplieerFormcontroller.java
@@ -1,4 +1,54 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+
 public class AddSupplieerFormcontroller {
+
+    public JFXTextField txtFirstName;
+    public JFXTextField txtCompany;
+    public Text txtEmailValidation;
+    public Text txtMobileNumberValidation;
+    public JFXTextField txtLastName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtMobileNumber;
+    public JFXButton btnResetPassword;
+    public TableView tblItems;
+    public TableColumn colItemId;
+    public TableColumn colItemName;
+    public Text txtSupplierID;
+    public JFXComboBox cmbItem;
+
+    public void btnMainmenuOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtEmailOnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtMobileNoOnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplierListOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateRemoveOnAction(ActionEvent actionEvent) {
+    }
 }
